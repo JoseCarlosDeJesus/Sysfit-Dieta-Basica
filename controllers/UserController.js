6a1b0e92bce6f7ea720c2913a06fd9c4426dd8e5
Leandro Vaguetti <104679202+leandrovaguetti@users.noreply.github.com>
2022-05-14 12:33:44 +0000

    Initial commit

---
+++ controllers/UserController.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const User = require('../models/User');
+
+module.exports = {
+
+   async store(req, res)
+     {
+        const user =  await User.create(req.body);
+
+        return res.json(user);
+     }
+ 
+};
